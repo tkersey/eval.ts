6e9998d07f04f65d9d5a0e17cb8b4eef6c26374a
Tim Kersey <t@k-t.im>
2025-06-20 17:04:15 -0700

    Trying to make it like another dependency used in a similar way

--- dist/metrics/g-eval/schemas.d.ts
+++ dist/metrics/g-eval/schemas.d.ts
@@ -5,9 +5,9 @@ import { z } from 'zod';
 export declare const EvaluationStepsSchema: z.ZodObject<{
     steps: z.ZodArray<z.ZodString, "many">;
 }, "strip", z.ZodTypeAny, {
-    steps: string[];
+    steps?: string[];
 }, {
-    steps: string[];
+    steps?: string[];
 }>;
 export type EvaluationStepsResponse = z.infer<typeof EvaluationStepsSchema>;
 /**
@@ -17,10 +17,10 @@ export declare const EvaluationResultSchema: z.ZodObject<{
     score: z.ZodNumber;
     reason: z.ZodString;
 }, "strip", z.ZodTypeAny, {
-    score: number;
-    reason: string;
+    score?: number;
+    reason?: string;
 }, {
-    score: number;
-    reason: string;
+    score?: number;
+    reason?: string;
 }>;
 export type EvaluationResult = z.infer<typeof EvaluationResultSchema>;
